a3105554e72266cfafe61379dc9b579bbf2675e8
noam_wsl <winnersalmostwin@gmail.com>
2025-01-06 23:09:18 -0500

    deleted raj

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,9 @@
 import './Footer.css'
 
 const Footer = () => (
-  <footer className='footer'>
-    <a
-      href='https://github.com/rjshkhr/cleanfolio'
-      className='link footer__link'
-    >
-      Created By Raj Shekhar
-    </a>
-  </footer>
+	<footer className='footer'>
+
+	</footer>
 )
 
 export default Footer
