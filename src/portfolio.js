ceb97347a108b55754de77a823f96dfe40c93f49
noam_wsl <winnersalmostwin@gmail.com>
2025-01-07 14:27:25 -0500

    making sure auto deploy works

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
 	name: 'Noam Salomonski',
 	role: 'Machine Learning Engineer',
 	description:
-		'Lorem ipsum dolor asit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 	resume: 'https://drive.google.com/file/d/1pGqA4FMvDAAbyah4DRYL0qMEoWFZG1Ag/view?usp=sharing',
 	social: {
 		linkedin: 'https://www.linkedin.com/in/noam-salomonski/',
