eb27b91c4aa64cf622b3fa5b3a1f30d96f4b3743
noam_wsl <winnersalmostwin@gmail.com>
2025-01-06 22:00:00 -0500

    copied the rest from the old

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,72 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+	// all the properties are optional - can be left empty or deleted
+	homepage: 'https://noamzilo.github.io/personal_website',
+	title: ' ',
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
-  social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
-  },
+	// all the properties are optional - can be left empty or deleted
+	name: 'Noam Salomonski',
+	role: 'Machine Learning Engineer',
+	description:
+		'I am a machine learning engineer with a big passion for building models that can solve real-world problems. I am a quick learner and I am always looking to expand my skills.',
+	resume: 'https://drive.google.com/file/d/1pGqA4FMvDAAbyah4DRYL0qMEoWFZG1Ag/view?usp=sharing',
+	social: {
+		linkedin: 'https://www.linkedin.com/in/noam-salomonski/',
+		github: 'https://github.com/noamzilo',
+		stackoverflow: 'https://stackoverflow.com/users/913098/gulzar',
+	},
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
-  {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 2',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
+	// projects can be added an removed
+	// if there are no projects, Projects section won't show up
+	{
+		name: 'Project 1',
+		description:
+			'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+		stack: ['SASS', 'TypeScript', 'React'],
+		sourceCode: 'https://github.com',
+		livePreview: 'https://github.com',
+	},
+	{
+		name: 'Project 2',
+		description:
+			'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+		stack: ['SASS', 'TypeScript', 'React'],
+		sourceCode: 'https://github.com',
+		livePreview: 'https://github.com',
+	},
+	{
+		name: 'Project 3',
+		description:
+			'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+		stack: ['SASS', 'TypeScript', 'React'],
+		sourceCode: 'https://github.com',
+		livePreview: 'https://github.com',
+	},
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+	// skills can be added or removed
+	// if there are no skills, Skills section won't show up
+	'HTML',
+	'CSS',
+	'JavaScript',
+	'TypeScript',
+	'React',
+	'Redux',
+	'SASS',
+	'Material UI',
+	'Git',
+	'CI/CD',
+	'Jest',
+	'Enzyme',
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+	// email is optional - if left empty Contact section won't show up
+	email: 'noamsalomonski@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
